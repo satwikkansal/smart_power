5d3a532f0edc6a3d5bf5aacc2a3997a18c200940
dushyantRathore <dushyant.bgs@gmail.com>
2017-12-02 18:37:25 +0530

    First Commit

---
+++ upload_units_raspi.py
@@ -0,0 +1,31 @@
+import requests
+import json
+import random
+import time
+
+meter_address = "1x11"
+
+i = 1
+
+while i <= 8:
+    data = {}
+    data['meter_address'] = meter_address
+    data['meter_usage'] = random.randint(5,15)
+    data['timestamp'] = time.time()
+
+    print data
+
+    time.sleep(1)
+
+    t = requests.post("http://127.0.0.1:5000/transactions/new", json=data)
+
+    print t.text
+
+    if i%4==0:
+        # Mine the pushed transactions for a single block
+        m = requests.get("http://127.0.0.1:5000/mine")
+        j_m = json.loads(m.text)
+
+    i += 1
+
+
